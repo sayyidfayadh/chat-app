35efd65f23bdb16440bd354d8279e895372935b2
sayyidfayadh <sayyidfayadh5@gmail.com>
2024-11-17 10:46:35 +0530

    more

--- src/components/Chatlist.jsx
+++ src/components/Chatlist.jsx
@@ -8,18 +8,16 @@ import { Button } from "react-bootstrap";
 import { useChatStore } from "../lib/chatStore";
 
 function Chatlist() {
-  
   const [chats, setChats] = useState([]);
-  const [addContact,setAddContact]=useState(false);
-  const { currentUser,fetchUserChats ,} = useUserStore();
-  const {selectChat,changeSeen}=useChatStore()
+  const [addContact, setAddContact] = useState(false);
+  const { currentUser, fetchUserChats } = useUserStore();
+  const { selectChat, changeSeen } = useChatStore();
   useEffect(() => {
     const unsub = onSnapshot(
-        //chat fetch
+      //chat fetch
 
       doc(db, "userchats", currentUser.id),
       async (res) => {
-      
         const items = res.data().chats;
         const promises = items.map(async (item) => {
           //userfetch
@@ -34,16 +32,15 @@ function Chatlist() {
         setChats(chatData.sort((a, b) => b.updatedAt - a.updatedAt));
       }
     );
-   
 
     return () => {
       unsub();
     };
   }, [currentUser.id]);
   console.log(chats);
-  const handleSelect=async (chat)=>{
+  const handleSelect = async (chat) => {
     const updatedChats = chats.map((item) => {
-      if (item.chatId === chat.chatId &&!item.isSeen) {
+      if (item.chatId === chat.chatId && !item.isSeen) {
         return {
           ...item,
           isSeen: true, // Mark the selected chat as seen
@@ -52,66 +49,86 @@ function Chatlist() {
       return item;
     });
     // console.log("hi");
-  setChats(updatedChats) 
-    const userChatsRef=doc(db,"userchats",currentUser.id)
+    setChats(updatedChats);
+    const userChatsRef = doc(db, "userchats", currentUser.id);
     try {
-      await updateDoc(userChatsRef,{
-        chats:updatedChats,
-        
-      })
+      await updateDoc(userChatsRef, {
+        chats: updatedChats.map(({ user, ...rest }) => rest),
+      });
       const otherUserChatsRef = doc(db, "userchats", chat.receiverId);
       const otherUserChatsSnap = await getDoc(otherUserChatsRef);
       if (otherUserChatsSnap.exists()) {
         const otherUserChats = otherUserChatsSnap.data().chats.map((item) =>
-          item.chatId === chat.chatId ? { ...item, isSeen: true } : item
-        );
+            item.chatId === chat.chatId ? { ...item, isSeen: true } : item
+          );
         await updateDoc(otherUserChatsRef, { chats: otherUserChats });
       }
-      selectChat(chat.chatId,chat.user)
+      selectChat(chat.chatId, chat.user);
     } catch (error) {
       console.log(error);
-      
     }
-    
-  }
+  };
   return (
     <>
-    
-    
-    <div className="chatlist p-2">
-       <div className="d-flex justify-content-between search ms-3">
-                {" "}
-                <img src="/media/search.png" width={"30px"} height={'35px'} alt="" className="me-2" />
-                <input
-                  type="text"
-                  className="form-control  w-100"
-                  placeholder=" search chat "
-                />
-                <Button className="ms-3 btn btn-light" onClick={()=>{setAddContact(!addContact)}} >{addContact?"-":"+"}</Button>{" "}
-                 {addContact?<AddUser/>:<></>}
-      </div>
-      <hr />
-      {chats?.map((chat) => (
-        <div className="onechat d-flex p-2 " key={chat.chatId} onClick={()=>handleSelect(chat)}>
+      <div className="chatlist p-2">
+        <div className="d-flex justify-content-between search ms-3">
+          {" "}
           <img
-            className="avi"
-            height={"40px"}
-            src={chat.user.avi || "/media/avatar.png"}
-            alt="sss"
+            src="/media/search.png"
+            width={"30px"}
+            height={"35px"}
+            alt=""
+            className="me-2"
           />
-          <p className="ms-4 " style={{ fontSize: "2.5vh" }}>
-            {chat.user.username} <br />
-            <span className="fs-6" style={{ fontWeight: "lighter" }}>
-     <div className="d-flex align-items-center gap-2">      
-       <i className="fa-solid fa-check-double fa-xs" style={{color:chat?.isSeen?"skyblue":"greenyellow"}}></i>
-     {chat?.lastMessage}</div> 
-            </span>{" "}
-          </p>
+          <input
+            type="text"
+            className="form-control  w-100"
+            placeholder=" search chat "
+          />
+          <Button
+            className="ms-3 btn btn-light"
+            onClick={() => {
+              setAddContact(!addContact);
+            }}
+          >
+            {addContact ? "-" : "+"}
+          </Button>{" "}
+          {addContact ? <AddUser /> : <></>}
         </div>
-      ))}
-    </div>
-  
-  </>);
+        <hr />
+        {chats?.map((chat) => (
+          <div
+            className="onechat d-flex p-2 "
+            key={chat.chatId}
+            onClick={() => handleSelect(chat)}
+          >
+            <img
+              className="avi"
+              height={"40px"}
+              src={chat.user.avi || "/media/avatar.png"}
+              alt="sss"
+            />
+            <p className="ms-4 " style={{ fontSize: "2.5vh" }}>
+              {chat.user.username} <br />
+              <span className="fs-6" style={{ fontWeight: "lighter" }}>
+                <div className="d-flex align-items-center gap-2">
+                  {
+                    <i
+                      className="fa-solid fa-check-double fa-xs"
+                      style={{
+                        color: chat?.isSeen ? "skyblue" : "greenyellow",
+                      }}
+                    ></i>
+                  }
+                  {chat?.lastMessage}
+                </div>
+              </span>{" "}
+            </p>
+          </div>
+        ))}
+      </div>
+    </>
+  );
 }
 
 export default Chatlist;
